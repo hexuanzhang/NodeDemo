1cf159751ad859d7a4ebefe19c83949f077bda9e
hexuanzhang <136789658@qq.com>
2017-08-17 18:25:27 +0800

    update

--- webstocket/index.js
+++ webstocket/index.js
@@ -9,20 +9,28 @@ const socket = new server({
 });
 
 socket.on('request', function(request) {
+	console.info('connection', socket.connections.length);
 	var connection = request.accept(null, request.origin);
 	
 	connection.on('message', function(message) {
 		console.log(message.utf8Data);
 		connection.sendUTF(JSON.stringify('hello'));
 		
-		Rx.Observable.interval(1000)
+		Rx.Observable.interval(3000)
 			.map(i => {
-				return Array.from({length: chance.integer({min: 1, max: 10})}, _ => chance.integer({min: 0, max: 100}) / 100);
+				return Array.from({length: chance.integer({min: 1, max: 10})}, (v, i) => {
+					let num = chance.integer({min: 0, max: 100}) / 100;
+					
+					return {
+						id: i,
+						name: `下载任务 ${i}`,
+						status: num,
+						date: new Date().toLocaleDateString()
+					}
+				});
 			})
 			.subscribe(data => {
-				const str = JSON.stringify({data});
-				console.info(data, str);
-				connection.sendUTF(str);
+				connection.sendUTF(JSON.stringify({data}));
 			});
 	});
 	
