6affb2d6a1e15be1ccd083f5df1ad95d8cf3b112
hexuanzhang <136789658@qq.com>
2018-03-22 18:38:44 +0800

    lesson2

---
+++ lesson/lesson2/app.js
@@ -0,0 +1,22 @@
+const express = require('express'),
+    utility = require('utility');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    const query = req.query,
+        keys = Object.keys(query);
+
+    if (Object.is(keys.length, 0) || !keys.includes('name')) {
+        res.send('缺少参数 name');
+    } else {
+        const method = query.method || 'md5', // 加密方法（md5, sha1, sha256）
+            format = query.format || 'base64'; // 输出字符串格式（hex、base64）
+
+        res.send(utility.hash(method, query.name, format));
+    }
+});
+
+app.listen('3001', () => {
+    console.info('服务已启动');
+});
