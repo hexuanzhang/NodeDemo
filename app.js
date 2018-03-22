fcdc2e1f41e0b7c4f84a7191f4c1c9f7c3edfd40
hexuanzhang <136789658@qq.com>
2018-03-22 16:38:43 +0800

    lesson1

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express'),
+    app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World.');
+});
+
+app.listen('3001', () => {
+    console.info('listen at port 30001');
+});
